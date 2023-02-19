c848bd038b9a2b722a24b09d25c4c21a6c1fbab2
John U <arjorb@gmail.com>
2023-02-19 11:44:15 +0200

    feat:creating a form for the meme generator app

---
+++ src/components/Meme.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Meme = () => {
+  return (
+    <>
+      <div className='my-10'>
+        <form action=''>
+          <div>
+            <input type='text' />
+            <input type='text' />
+          </div>
+          <button>Get a new meme image 🖼</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Meme;
