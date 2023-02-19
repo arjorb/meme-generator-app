4755522dd6fe487765fb711a6a710b8b6fbd03a4
John U <arjorb@gmail.com>
2023-02-19 12:10:22 +0200

    fix:done fixing the placeholder for the text inputs

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,8 +6,16 @@ const Meme = () => {
       <div className='my-10'>
         <form action='' className='flex flex-col w-10/12 mx-auto'>
           <div className='flex gap-2 justify-center'>
-            <input type='text' className='py-1 pl-1 text-lg outline-none rounded-md border-2 border-gray-200 w-full' />
-            <input type='text' className='py-1 pl-1 text-lg outline-none rounded-md border-2 border-gray-200 w-full' />
+            <input
+              type='text'
+              placeholder='Top text'
+              className='py-1 pl-1 text-lg outline-none rounded-md border-2 border-gray-200 w-full'
+            />
+            <input
+              type='text'
+              placeholder='Bottom text'
+              className='py-1 pl-1 text-lg outline-none rounded-md border-2 border-gray-200 w-full'
+            />
           </div>
           <button className='w-full rounded-md my-5 py-3 px-6 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
             Get a new meme image 🖼
