0dfd1ce936cddf77315af4fe552c63fde50daa70
John U <arjorb@gmail.com>
2023-03-02 12:45:01 +0200

    removing all the unncessary file

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className=''>
-      <h1 className='text-3xl text-[#03948d] font-bold underline'>Seting up the environment</h1>
-    </div>
-  );
-}
-
-export default App;
